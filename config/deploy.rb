9f63444c4dc9137d07f3f5703732fa3cd40ea726
weih <xiaohaoprog@gmail.com>
2012-05-12 23:26:09 +0800

    nginx.con edited

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,8 +17,19 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 set :rvm_type, :user
+
+namespace :deploy do
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+  end
+  after "deploy:setup", "deploy:setup_config"
+end
+
+
 set :default_environment, {
-  'PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p194/bin/:$PATH",
+  'PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p194/bin:/usr/local/rvm/gems/ruby-1.9.3-p194@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p194/bin:/usr/local/rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
+  'RUBY_VERSION' => 'ruby-1.9.3-p194',
   'GEM_HOME' => '/usr/local/rvm/gems/ruby-1.9.3-p194',
   'GEM_PATH' => '/usr/local/rvm/gems/ruby-1.9.3-p194:/usr/local/rvm/gems/ruby-1.9.3-p194@global'
 }
