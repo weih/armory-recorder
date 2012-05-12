7488b23ef1de2d624511033a4c4f73d2268f2d74
weih <xiaohaoprog@gmail.com>
2012-05-13 00:50:03 +0800

    edited add .html suffix from target_path attribute

--- app/models/character.rb
+++ app/models/character.rb
@@ -85,8 +85,8 @@ class Character < ActiveRecord::Base
     y, m, d = t.year, t.month, t.day
 
     FileUtils.makedirs("public/zh/#{server}/#{name}/#{y}/#{m}")
-    target_path = "/zh/#{server}/#{name}/#{y}/#{m}/#{d}"
-    [target_path, "public" + target_path + ".html"]
+    target_path = "/zh/#{server}/#{name}/#{y}/#{m}/#{d}.html"
+    [target_path, "public" + target_path]
   end
 
   def make_new_history(doc, target_path, last_update)  
