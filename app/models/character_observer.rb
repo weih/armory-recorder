148032a3c4a3fe266eeff219ed5751bf029022ea
weih <xiaohaoprog@gmail.com>
2012-05-23 14:45:24 +0800

    send mail when record new character

--- app/models/character_observer.rb
+++ app/models/character_observer.rb
@@ -1,6 +1,10 @@
 class CharacterObserver < ActiveRecord::Observer
   def after_save(model)
     puts "New Character: #{model.name} from #{model.server}"
-    CharacterMailer.delay.new_record(model)
+    if model.created_at.to_date == Date.today.to_date
+      CharacterMailer.delay.new_record(model)
+    else
+      puts "Fresh Old Character."
+    end
   end
 end
--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -1,1 +1,2 @@
-Slim::Engine.set_default_options :pretty => true, :tabsize => 2
+# Slim::Engine.set_default_options :pretty => true, :tabsize => 2
+Slim::Engine.set_default_options :tabsize => 2
