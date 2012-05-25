0a91d8e0e4036392d3787d83c7967034db030b14
weih <xiaohaoprog@gmail.com>
2012-05-25 20:03:01 +0800

    add avatar to mailer

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,8 @@ module Armory
 
     config.action_mailer.delivery_method = :smtp
 
+    config.action_mailer.default_url_options = { host: "wowarmory.me" }
+
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
